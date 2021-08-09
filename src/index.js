60575b1277f88bb46b71ff966d6862cdcd8c8d69
[Valentina] <[mariaiscus1@gmail.com]>
2021-08-09 18:26:48 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined ||  array.length === 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined ||  array.length === 0) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined ||  array.length === 0) return 0;
+  return (array.reduce((amount, item) => amount + item)) / array.length;
 }
